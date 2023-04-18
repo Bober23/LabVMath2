09641d8d674b22eddfc3a6655dd01b1090d9bafb
22222 11111 <vanya.popov.2003@gmail.com>
2023-04-18 09:40:00 +0300

    Final

--- LabVMath2/LabTask.cs
+++ LabVMath2/LabTask.cs
@@ -26,13 +26,33 @@ namespace LabVMath2
             points[5] = new MyPoint(1,0.94);
             h = new double[] { points[1].x - points[0].x, points[2].x - points[1].x, points[3].x - points[2].x, points[4].x - points[3].x, points[5].x - points[4].x };
         }
+        public double[] GetACoefs()
+        {
+            double[] a = new double[4];
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = points[i].y;
+            }
+            return a;
+        }
+        public double[] GetBCoefs()
+        {
+            double[] c = GetCCoefs();
+            double[] b = new double[4];
+            for (int i = 0;i < b.Length - 1; i++)
+            {
+                b[i] = ((points[i + 1].y - points[i].y) / h[i]) - 1.0 / 3.0 * h[i] * (c[i + 1] + 2 * c[i]);
+                }
+            b[b.Length - 1] = ((points[b.Length].y - points[b.Length - 1].y) / h[b.Length - 1]) - 2.0 / 3.0 * h[b.Length - 1] * c[b.Length - 1];
+            return b;
+        }
         public double[] GetCCoefs()
         {
             double[,] matrix = GetSplineMatrix();
             double[] p = new double[4];
             double[] q = new double[4];
             double[] x = new double[4];
-            p[0] = -matrix[0,2] / (matrix[0,1] + matrix[0,0]);
+            p[0] = -matrix[0, 2] / (matrix[0, 1] + matrix[0, 0]);
             for (int i = 1; i < p.Length; i++)
             {
                 p[i] = -matrix[i, i + 2] / (matrix[i, i + 1] + matrix[i, i] * p[i - 1]);
@@ -42,13 +62,24 @@ namespace LabVMath2
             {
                 q[i] = (matrix[i, 6] - matrix[i, i] * q[i - 1]) / (matrix[i, i + 1] + matrix[i, i] * p[i - 1]);
             }
-            x[x.Length-1] = q[q.Length-1];
+            x[x.Length - 1] = q[q.Length - 1];
             for (int i = x.Length - 2; i >= 0; i--)
             {
                 x[i] = p[i] * x[i + 1] + q[i];
             }
             return x;
 
+        }
+        public double[] GetDCoefs()
+        {
+            double[] c = GetCCoefs();
+            double[] d = new double[4];
+            for (int i = 0; i < d.Length - 1; i++)
+            {
+                d[i] = (c[i + 1] - c[i]) / 3 * h[i];
+            }
+            d[d.Length - 1] = -c[d.Length - 1] / 3 * h[d.Length - 1];
+            return d;
         }
         private double[,] GetSplineMatrix()
         {
--- LabVMath2/Program.cs
+++ LabVMath2/Program.cs
@@ -4,7 +4,22 @@ using LabVMath2;
 Console.WriteLine("Hello, World!");
 var MyTask = new LabTask();
 double[] cCoefs = MyTask.GetCCoefs();
+double[] aCoefs = MyTask.GetACoefs();
+double[] bCoefs = MyTask.GetBCoefs();
+double[] dCoefs = MyTask.GetDCoefs();
+foreach (var a in aCoefs)
+{
+    Console.WriteLine($"A = {a}");
+}
+foreach (var b in bCoefs)
+{
+    Console.WriteLine($"B = {b}");
+}
 foreach (var c in cCoefs)
 {
-    Console.WriteLine(c);
+    Console.WriteLine($"C = {c}");
+}
+foreach (var d in dCoefs)
+{
+    Console.WriteLine($"D = {d}");
 }
